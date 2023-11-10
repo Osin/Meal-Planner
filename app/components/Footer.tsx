dea5dfed7c73df3340a32256cea7366ea28b4c29
Housseini Toumani <osin@live.com>
2023-11-10 17:39:38 +0100

    feat: add Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,18 @@
 import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 const footer = () => (
-    <AppBar position="sticky" color="primary"><Toolbar variant={'dense'}>Copyright 2023 - Housseini Toumani</Toolbar></AppBar>
-);
+    <AppBar position="sticky" color="primary"><Toolbar variant={'dense'}>
+          <Typography>Copyright 2023 - Housseini Toumani</Typography>
+          <Typography>
+                <a href="https://www.flaticon.com/free-icons/lunch" title="lunch icons">
+                      Lunch icons created by Linector - Flaticon
+                </a>
+          </Typography>
+    </Toolbar>
+</AppBar>
+)
+;
 export default footer;
